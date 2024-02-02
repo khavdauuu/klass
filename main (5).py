955088467f8f5005c0172e7046a16cea128b811f
khavdauuu <157492006+khavdauuu@users.noreply.github.com>
2024-02-02 11:03:22 +0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,9 @@
+tseloe = 10
+drobnoe = 8.4 
+stroka = "No"
+big_netseloe = tseloe * 3.5
+drobnoe = drobnoe - 1 
+print(tseloe/drobnoe, big_netseloe/drobnoe)
+stroka11 = "Yes"
+print(stroka * 2 + "_" + stroka11 * 3)
+print(tseloe, drobnoe, stroka, big_netseloe, stroka11)
