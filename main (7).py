90f076757c72d55bbdee51b459402deaa8543e84
khavdauuu <157492006+khavdauuu@users.noreply.github.com>
2024-02-02 11:50:19 +0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,14 @@
+num = 23
+num1 = 82
+print(num > 20 and num1 != 28)
+print(not num > 23 and not num1 < 82)
+print(num == 25 and num1 == 82)
+print(num >= 23 and not num != 82)
+print(num > 20 or num1 != 28)
+print(not num > 23 or not num1 < 82)
+print(num == 25 or num1 != 82)
+print(not num >= 23 or not num != 82)
+
+str = "8"
+str22 = "129"
+print(str != "10" and str22 == "129")
