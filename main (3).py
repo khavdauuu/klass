564d6c68d5030aeda68bde474edfbcc0604448dc
khavdauuu <157492006+khavdauuu@users.noreply.github.com>
2024-02-16 11:46:54 +0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,12 @@
+a = int(input("Введите количсевто чисел ряда Фибоначчи: "))
+fib1 = 0
+fib2 = 1
+print(fib1)
+print(fib2)
+i = 2
+while i < a:
+    s = fib1 + fib2
+    print(s)
+    fib1 = fib2
+    fib2 = s
+    i += 1
